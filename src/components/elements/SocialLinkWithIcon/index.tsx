3d7f35a4ca74973c58f53221dc013596db9e115f
8ayac <8ayac.y@gmail.com>
2021-01-24 03:05:28 +0900

    :new: Add SocialLinkWithIcon component.

---
+++ src/components/elements/SocialLinkWithIcon/index.tsx
@@ -0,0 +1,41 @@
+import { theme } from '@/src/constants/theme';
+import styled from '@emotion/styled';
+import { faLink, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const S = {
+  ComponentWrapperDiv: styled.div`
+    margin: 0 0.5rem;
+  `,
+
+  SocialLinkAnchor: styled.a`
+    font-size: 2.2rem;
+    color: ${(props) => props.color || 'inherit'};
+    text-decoration: none;
+
+    &:hover {
+      color: ${theme.color.yellow.base};
+    }
+  `,
+
+  LinkDescriptionSpan: styled.span`
+    margin-left: 0.5rem;
+  `,
+};
+
+export const SocialLinkWithIcon: React.FC<{
+  url: string;
+  description: string;
+  icon?: IconDefinition;
+  color?: string;
+}> = ({ url, description, icon = faLink, color }) => (
+  <>
+    <S.ComponentWrapperDiv>
+      <S.SocialLinkAnchor href={url} color={color}>
+        <FontAwesomeIcon icon={icon} />
+        <S.LinkDescriptionSpan>{description}</S.LinkDescriptionSpan>
+      </S.SocialLinkAnchor>
+    </S.ComponentWrapperDiv>
+  </>
+);
