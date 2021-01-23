8cdb977e8bc54d7e5c70760eeac78662f8b737ac
8ayac <8ayac.y@gmail.com>
2021-01-23 21:33:30 +0900

    :wrench: Add some settings so we can test components.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   verbose: true,
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/enzyme.setup.ts'],
+  snapshotSerializers: ['@emotion/jest/enzyme-serializer'],
 };
