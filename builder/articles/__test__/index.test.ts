842215d2e13046318dc882a1abd6c6f686746cf6
8ayac <8ayac.y@gmail.com>
2021-01-26 06:34:06 +0900

    :recycle: Remove 'TEST_ARTICLES' and add 'TESTDATA_DIR'. Add more generic constant and remove that which isn't.

--- builder/articles/__test__/index.test.ts
+++ builder/articles/__test__/index.test.ts
@@ -11,11 +11,11 @@ const readFileSyncAsJSON = (fpath: string) => {
   return JSON.parse(fs.readFileSync(fpath, { encoding: 'utf8' }));
 };
 
-const TEST_ARTICLES_DIR = path.join(__dirname, 'testdata/articles');
+const TESTDATA_DIR = path.join(__dirname, 'testdata');
 
 describe('getDirNamesIn', () => {
   it('can get names of directories that contains each article', () => {
-    expect(getDirNamesIn(TEST_ARTICLES_DIR)).toEqual([
+    expect(getDirNamesIn(path.join(TESTDATA_DIR, 'articles'))).toEqual([
       'example01',
       'example02',
       'example03',
@@ -24,27 +24,25 @@ describe('getDirNamesIn', () => {
 });
 
 describe('parseMarkdownWithMeta', () => {
-  const mdPath = path.join(TEST_ARTICLES_DIR, 'example01', 'index.md');
+  const mdPath = path.join(TESTDATA_DIR, 'articles', 'example01', 'index.md');
   const mdRawContent = fs.readFileSync(mdPath).toString();
   const parsed = parseMarkdownWithMeta(mdRawContent);
 
   it('can parse meta attributes and body from the Markdown file', () => {
-    expect(parsed).toEqual({
+    expect(parsed).toMatchObject({
       id: 'example01',
       title: 'Example01',
       publishedAt: '2000-01-01 00:00:00 +0900',
       updatedAt: '2000-01-01 00:00:00 +0900',
       tags: ['example1-1', 'example1-2', 'example1-3'],
-      body: expect.stringMatching(
-        /^##\sexample1-1(\r?\n){2}bluhbluhbluh(\r?\n){2}##\slink(\r?\n){2}-\s<https:\/\/8ay\.ac>(\r?\n){2}##\simage(\r?\n){2}!\[blue]\(img\/blue\.png\)(\r?\n)!\[red]\(img\/red\.png\)(\r?\n)+$/
-      ),
+      body: /^##\sexample1-1(\r?\n){2}bluhbluhbluh(\r?\n){2}##\slink(\r?\n){2}-\s<https:\/\/8ay\.ac>(\r?\n){2}##\simage(\r?\n){2}!\[blue]\(img\/blue\.png\)(\r?\n)!\[red]\(img\/red\.png\)(\r?\n)+$/,
     });
   });
 });
 
 describe('copyImageDirectories', () => {
-  const testArticleDir = path.join(TEST_ARTICLES_DIR, 'example01');
-  const testPublicRoot = path.join(TEST_ARTICLES_DIR, '..', '.public');
+  const testArticleDir = path.join(TESTDATA_DIR, 'articles', 'example01');
+  const testPublicRoot = path.join(TESTDATA_DIR, '.public');
 
   const testArticleId = parseMarkdownWithMeta(
     fs.readFileSync(path.join(testArticleDir, 'index.md')).toString()
@@ -67,23 +65,24 @@ describe('copyImageDirectories', () => {
 });
 
 describe('build', () => {
-  const articlesDir = path.join(__dirname, 'testdata/articles');
-  const outputDir = path.join(__dirname, 'testdata/.contents');
-  const outputFileName = 'articles.test.json';
+  const testArticlesDir = path.join(TESTDATA_DIR, 'articles');
+  const testOutputDir = path.join(TESTDATA_DIR, '.contents');
+  const testOutputFileName = 'articles.test.json';
 
   beforeAll(() => {
-    build(articlesDir, outputDir, outputFileName);
+    build(testArticlesDir, testOutputDir, testOutputFileName);
   });
 
   afterAll(() => {
-    fs.rmdirSync(outputDir, { recursive: true });
+    fs.rmdirSync(testOutputDir, { recursive: true });
   });
 
   it('can generate a json which contains all the article data', () => {
     expect(
-      readFileSyncAsJSON(path.join(outputDir, outputFileName))
+      readFileSyncAsJSON(path.join(testOutputDir, testOutputFileName))
     ).toMatchObject([
       {
+        id: 'example01',
         title: 'Example01',
         publishedAt: '2000-01-01 00:00:00 +0900',
         updatedAt: '2000-01-01 00:00:00 +0900',
@@ -91,6 +90,7 @@ describe('build', () => {
         body: /^##\sexample1-1(\r?\n){2}bluhbluhbluh(\r?\n){2}##\slink(\r?\n){2}-\s<https:\/\/8ay\.ac>(\r?\n){2}##\simage(\r?\n){2}!\[blue]\(img\/blue\.png\)(\r?\n)!\[red]\(img\/red\.png\)(\r?\n)+$/,
       },
       {
+        id: 'example02',
         title: 'Example02',
         publishedAt: '2000-01-02 00:00:00 +0900',
         updatedAt: '2000-01-02 00:00:00 +0900',
@@ -98,6 +98,7 @@ describe('build', () => {
         body: /^##\sexample2-1(\r?\n){2}bluhbluhbluh(\r?\n){2}##\slink(\r?\n){2}-\s<https:\/\/8ay\.ac>(\r?\n){2}##\simage(\r?\n){2}!\[blue]\(img\/blue02\.png\)(\r?\n)!\[red]\(img\/red02\.png\)(\r?\n)+$/,
       },
       {
+        id: 'example03',
         title: 'Example03',
         publishedAt: '2000-01-03 00:00:00 +0900',
         updatedAt: '2000-01-03 00:00:00 +0900',
