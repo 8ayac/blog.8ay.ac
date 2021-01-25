2f673d03d6c7bae86dfd7e2ca28c123f2ec114e0
8ayac <8ayac.y@gmail.com>
2021-01-25 10:20:22 +0900

    :truck: Rename 'types/index.ts' to 'types/article.ts'

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,1 @@
-export type ArticleAttribute = {
-  title: string;
-  publishedAt: Date;
-  updatedAt: Date;
-  tags?: string[];
-};
-
-export type Article = ArticleAttribute & {
-  body: string;
-};
+export * from './article';
