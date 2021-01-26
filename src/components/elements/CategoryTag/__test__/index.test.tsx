d6642995859ad8e7dbf3ef4ee6339ed78e72b83a
8ayac <8ayac.y@gmail.com>
2021-01-27 06:35:58 +0900

    :sparkles: Add CategoryTag component.

---
+++ src/components/elements/CategoryTag/__test__/index.test.tsx
@@ -0,0 +1,51 @@
+import { CategoryTag } from '@/src/components/elements/CategoryTag';
+import { theme } from '@/src/constants/theme';
+import { mount, shallow } from 'enzyme';
+import React from 'react';
+
+describe('CategoryTag', () => {
+  const testCategoryName = 'test-cat';
+
+  test('is rendered correctly to match the snapshot', () => {
+    const wrapper = mount(<CategoryTag name={testCategoryName} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('has the correct text', () => {
+    const wrapper = mount(<CategoryTag name={testCategoryName} />);
+    expect(wrapper.find('a').text()).toBe(testCategoryName);
+  });
+
+  test('links properly to `/category/:name`', () => {
+    const wrapper = mount(<CategoryTag name={testCategoryName} />);
+    expect(wrapper.find('a').prop('href')).toEqual('/category/test-cat');
+  });
+
+  describe('has proper style rules', () => {
+    test('in Anchor', () => {
+      const wrapper = shallow(<CategoryTag name={testCategoryName} />);
+      expect(wrapper.find('Anchor')).toHaveStyleRule('padding', '3% 1rem 4%');
+      expect(wrapper.find('Anchor')).toHaveStyleRule('margin', '0');
+      expect(wrapper.find('Anchor')).toHaveStyleRule('font-size', '0.8em');
+      expect(wrapper.find('Anchor')).toHaveStyleRule('font-weight', '600');
+      expect(wrapper.find('Anchor')).toHaveStyleRule('color', 'white');
+      expect(wrapper.find('Anchor')).toHaveStyleRule('text-decoration', 'none');
+      expect(wrapper.find('Anchor')).toHaveStyleRule(
+        'background-color',
+        theme.color.blue.dark
+      );
+      expect(wrapper.find('Anchor')).toHaveStyleRule('border-radius', '3px');
+
+      expect(wrapper.find('Anchor')).toHaveStyleRule('color', 'white', {
+        target: ':hover',
+      });
+      expect(wrapper.find('Anchor')).toHaveStyleRule(
+        'background-color',
+        theme.color.blue.base,
+        {
+          target: ':hover',
+        }
+      );
+    });
+  });
+});
---
+++ src/components/elements/CategoryTag/index.tsx
@@ -0,0 +1,31 @@
+import { theme } from '@/src/constants/theme';
+import { getCategoryPagePath } from '@/src/shared/utils';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import React from 'react';
+
+const S = {
+  Anchor: styled.a`
+    padding: 3% 1rem 4%;
+    margin: 0;
+    font-size: 0.8em;
+    font-weight: 600;
+    color: white;
+    text-decoration: none;
+    background-color: ${theme.color.blue.dark};
+    border-radius: 3px;
+
+    &:hover {
+      color: white;
+      background-color: ${theme.color.blue.base};
+    }
+  `,
+};
+
+export const CategoryTag: React.FC<{ name: string }> = ({ name }) => (
+  <>
+    <Link href={getCategoryPagePath(name)} passHref>
+      <S.Anchor>{name}</S.Anchor>
+    </Link>
+  </>
+);
