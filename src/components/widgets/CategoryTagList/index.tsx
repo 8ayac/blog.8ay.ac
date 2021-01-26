c48c32c4975034f80e6b3991613f8a4ac795e3ef
8ayac <8ayac.y@gmail.com>
2021-01-27 06:36:19 +0900

    :new: Add CategoryTagList component.

---
+++ src/components/widgets/CategoryTagList/index.tsx
@@ -0,0 +1,20 @@
+import { CategoryTag } from '@/src/components/elements/CategoryTag';
+import styled from '@emotion/styled';
+import React from 'react';
+
+const S = {
+  ComponentWrapperDiv: styled.div`
+    display: inline-flex;
+    margin: 0 0.5rem 0.5rem 0;
+  `,
+};
+
+export const CategoryTagList: React.FC<{ tags: string[] }> = ({ tags }) => (
+  <>
+    {[...new Set(tags)].sort().map((tag) => (
+      <S.ComponentWrapperDiv key={tag}>
+        <CategoryTag name={tag} />
+      </S.ComponentWrapperDiv>
+    ))}
+  </>
+);
