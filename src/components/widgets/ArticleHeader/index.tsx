af558545e693d03d1dbd18e1595bddf52596e84a
8ayac <8ayac.y@gmail.com>
2021-01-27 00:27:27 +0900

    :new: Add BodyHeader component. This component is more generalized component which is used on each page.

--- src/components/widgets/ArticleHeader/index.tsx
+++ src/components/widgets/ArticleHeader/index.tsx
@@ -1,38 +1,31 @@
+import { BodyHeader } from '@/src/components/elements/BodyHeader';
 import { DateTime } from '@/src/components/elements/DateTime';
-import { theme } from '@/src/constants/theme';
 import { FlexContainerDiv } from '@/src/shared/styles/abstractStyledComponents';
 import { Article } from '@/src/types';
 import styled from '@emotion/styled';
 import React from 'react';
 
 const S = {
-  WrapperHeader: styled.header`
-    border-bottom: 0.25rem solid ${theme.color.border.primaryLight};
-    margin-bottom: 3rem;
-  `,
-
-  ArticleTitleH1: styled.h1`
-    font-size: 2.16em;
-    margin: 0 0 1rem;
-  `,
-
   DateTimeWrapperDiv: styled(FlexContainerDiv)`
     align-items: center;
     flex-flow: row wrap;
 
     > * {
-      margin-bottom: 0.3rem;
       margin-right: 1rem;
     }
   `,
 };
 
-export const ArticleHeader: React.FC<{ article: Article }> = ({ article }) => (
-  <S.WrapperHeader>
-    <S.DateTimeWrapperDiv>
-      <DateTime date={article.publishedAt} description="Published At" />
-      <DateTime date={article.updatedAt} description="Updated At" />
-    </S.DateTimeWrapperDiv>
-    <S.ArticleTitleH1>{article.title}</S.ArticleTitleH1>
-  </S.WrapperHeader>
-);
+export const ArticleHeader: React.FC<{ article: Article }> = ({ article }) => {
+  return (
+    <BodyHeader
+      title={article.title}
+      beforeTitle={
+        <S.DateTimeWrapperDiv>
+          <DateTime date={article.publishedAt} description="Published At" />
+          <DateTime date={article.updatedAt} description="Updated At" />
+        </S.DateTimeWrapperDiv>
+      }
+    />
+  );
+};
--- src/components/widgets/ArticleHeader/test/index.test.tsx
+++ src/components/widgets/ArticleHeader/test/index.test.tsx
@@ -1,7 +1,6 @@
 import { ArticleHeader } from '@/src/components/widgets/ArticleHeader';
-import { theme } from '@/src/constants/theme';
 import { Article } from '@/src/types';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 
 describe('ArticleHeader', () => {
@@ -16,35 +15,13 @@ describe('ArticleHeader', () => {
 
   describe('is rendered correctly', () => {
     test(' to match the snapshot', () => {
-      const wrapper = shallow(<ArticleHeader article={testArticleData} />);
+      const wrapper = mount(<ArticleHeader article={testArticleData} />);
       expect(wrapper).toMatchSnapshot();
     });
   });
 
   describe('has proper style rules', () => {
-    const wrapper = shallow(<ArticleHeader article={testArticleData} />);
-
-    test('in WrapperHeader', () => {
-      expect(wrapper.find('WrapperHeader')).toHaveStyleRule(
-        'border-bottom',
-        `0.25rem solid ${theme.color.border.primaryLight}`
-      );
-      expect(wrapper.find('WrapperHeader')).toHaveStyleRule(
-        'margin-bottom',
-        '3rem'
-      );
-    });
-
-    test('in ArticleTitleH1', () => {
-      expect(wrapper.find('ArticleTitleH1')).toHaveStyleRule(
-        'font-size',
-        '2.16em'
-      );
-      expect(wrapper.find('ArticleTitleH1')).toHaveStyleRule(
-        'margin',
-        '0 0 1rem'
-      );
-    });
+    const wrapper = mount(<ArticleHeader article={testArticleData} />);
 
     test('in DateTimeWrapperDiv', () => {
       expect(wrapper.find('DateTimeWrapperDiv')).toHaveStyleRule(
@@ -56,13 +33,6 @@ describe('ArticleHeader', () => {
         'row wrap'
       );
 
-      expect(wrapper.find('DateTimeWrapperDiv')).toHaveStyleRule(
-        'margin-bottom',
-        '0.3rem',
-        {
-          target: '*',
-        }
-      );
       expect(wrapper.find('DateTimeWrapperDiv')).toHaveStyleRule(
         'margin-right',
         '1rem',
