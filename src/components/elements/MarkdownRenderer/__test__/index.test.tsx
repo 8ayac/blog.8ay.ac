0efd57ce8347e4d75bb6f796cc9bcb5e108fa9d4
8ayac <8ayac.y@gmail.com>
2021-01-30 08:45:31 +0900

    :recycle: Fix the wrong component name.

--- src/components/elements/MarkdownRenderer/__test__/index.test.tsx
+++ src/components/elements/MarkdownRenderer/__test__/index.test.tsx
@@ -1,16 +1,16 @@
-import { MarkdownRender } from '@/src/components/elements/MarkdownRenderer';
+import { MarkdownRenderer } from '@/src/components/elements/MarkdownRenderer';
 import { theme } from '@/src/constants/theme';
 import { ThemeProvider } from '@emotion/react';
 import { mount } from 'enzyme';
 import React from 'react';
 
-describe('MarkdownRender', () => {
+describe('MarkdownRenderer', () => {
   test('is rendered correctly to match the snapshot', () => {
     // It is not responsible for me to test whether the Markdown content is properly converted to HTML.
     // So, here I test only that the style rules applied to generated HTML will not be changed unintentionally, using a snapshot.
     const wrapper = mount(
       <ThemeProvider theme={theme}>
-        <MarkdownRender content="test" />
+        <MarkdownRenderer content="test" />
       </ThemeProvider>
     );
     expect(wrapper).toMatchSnapshot();
