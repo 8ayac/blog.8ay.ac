3243ce43a1fe999de10b019632a48c46966d1f9f
8ayac <8ayac.y@gmail.com>
2021-01-31 08:00:32 +0900

    :new: Add 'ArticleRevisionRecordList' component.

---
+++ src/components/elements/ArticleRevisionRecordList/index.tsx
@@ -0,0 +1,83 @@
+import { REPOSITORY_URL } from '@/src/constants/site';
+import { ArticleChangeLog } from '@/src/types';
+import styled from '@emotion/styled';
+import React from 'react';
+import urljoin from 'url-join';
+
+const S = {
+  Details: styled.details``,
+
+  Summary: styled.summary`
+    margin-bottom: 0.75rem;
+    font-weight: 600;
+    cursor: pointer;
+    outline: none;
+  `,
+
+  LogList: styled.ul`
+    padding-left: 0;
+    margin: 0;
+  `,
+
+  LogListItem: styled.li`
+    margin-bottom: 0.75rem;
+    list-style: none;
+  `,
+
+  UpdateDateTime: styled.time``,
+
+  UpdateDescriptionSpan: styled.span`
+    font-weight: bolder;
+  `,
+
+  ToGitHubCommitAnchor: styled.a`
+    font-weight: 600;
+  `,
+};
+
+const removeGitCommitPrefix = (commitMessage: string): string => {
+  return commitMessage.replace(/^:.*?:\s+/, '');
+};
+
+const getGitCommitUrl = (commitId: string): string => {
+  return urljoin(REPOSITORY_URL, 'commit', commitId);
+};
+
+const LogList: React.FC<{ log: ArticleChangeLog[] }> = ({ log }) => (
+  <S.LogList>
+    {log.map((record) => (
+      <S.LogListItem key={record.id}>
+        <S.UpdateDateTime>{record.date.toISOString()}</S.UpdateDateTime>{' '}
+        <S.UpdateDescriptionSpan>
+          {removeGitCommitPrefix(record.description)}
+        </S.UpdateDescriptionSpan>{' '}
+        (
+        <S.ToGitHubCommitAnchor
+          href={getGitCommitUrl(record.id)}
+          target="_blank"
+          rel="noopener"
+        >
+          #{record.id}
+        </S.ToGitHubCommitAnchor>{' '}
+        by {record.author})
+      </S.LogListItem>
+    ))}
+  </S.LogList>
+);
+
+export const ArticleRevisionRecordList: React.FC<{
+  log: ArticleChangeLog[];
+}> = ({ log }) => {
+  log.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+  if (log.length < 1) return <></>;
+
+  return (
+    <>
+      <S.Details open>
+        <S.Summary>Revision History</S.Summary>
+        <LogList log={log} />
+      </S.Details>
+    </>
+  );
+};
--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,2 +1,4 @@
 export const BLOG_TITLE = 'もしくはこれ';
 export const BLOG_SUBTITLE = "8ayac's blog";
+
+export const REPOSITORY_URL = 'https://github.com/8ayac/blog.8ay.ac';
