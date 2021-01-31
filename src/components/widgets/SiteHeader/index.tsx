6501766ed5a89960d162aa4da0d4d1edaa73a434
8ayac <8ayac.y@gmail.com>
2021-01-31 13:22:56 +0900

    :art: Modify SiteHeader component style.

--- src/components/widgets/SiteHeader/index.tsx
+++ src/components/widgets/SiteHeader/index.tsx
@@ -1,44 +1,44 @@
 import { BlogTitle } from '@/src/components/elements/BlogTitle';
 import { SocialLinkWithIcon } from '@/src/components/elements/SocialLinkWithIcon';
 import { BLOG_SUBTITLE, BLOG_TITLE } from '@/src/constants/site';
-import { FlexContainerDiv } from '@/src/shared/styles/abstractStyledComponents';
-import styled from '@emotion/styled';
+import styled, { StyledComponent } from '@emotion/styled';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 
-const S = {
+const S: { [key: string]: StyledComponent<any> } = {
   Header: styled.header`
     position: sticky;
     top: 0;
     z-index: 100;
-    height: 3.7rem;
-    padding: 0.5rem 1rem;
-    background-color: ${(props) => props.theme.color.green.dark2};
-    border-bottom: 1px solid ${(props) => props.theme.color.green.light};
-  `,
 
-  HeaderInner: styled.div`
     display: flex;
     align-items: center;
+
+    min-height: 5rem;
+    padding: 0.5rem 1.5rem;
+
+    background-color: ${(props) => props.theme.color.green.dark3};
+    filter: drop-shadow(0 0 2px ${(props) => props.theme.color.green.dark4});
+    opacity: 0.9;
+  `,
+
+  HeaderInner: styled.nav`
+    display: flex;
     justify-content: space-between;
-    height: 100%;
+    width: 100%;
     color: white;
   `,
 
-  TitleWrapper: styled(FlexContainerDiv)`
-    align-items: center;
+  InnerWrapperFlexContainerDiv: styled.div`
+    display: flex;
     filter: drop-shadow(
-      2px 2px 1px ${(props) => props.theme.color.green.dark3}
+      2px 2px 1px ${(props) => props.theme.color.green.dark4}
     );
-  `,
-
-  NavWrapper: styled(FlexContainerDiv)`
-    align-items: center;
-    filter: drop-shadow(
-      2px 2px 1px ${(props) => props.theme.color.green.dark3}
-    );
+    opacity: 1;
   `,
 };
+S.TitleWrapper = styled(S.InnerWrapperFlexContainerDiv)``;
+S.NavWrapper = styled(S.InnerWrapperFlexContainerDiv)``;
 
 export const SiteHeader: React.FC = () => (
   <S.Header>
