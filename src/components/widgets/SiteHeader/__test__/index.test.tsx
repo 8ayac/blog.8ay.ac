f3b2c1665b0d14382e678eb7b0254102b49eb595
8ayac <8ayac.y@gmail.com>
2021-01-31 13:22:56 +0900

    :art: Modify SiteHeader component style.

--- src/components/widgets/SiteHeader/__test__/index.test.tsx
+++ src/components/widgets/SiteHeader/__test__/index.test.tsx
@@ -22,58 +22,63 @@ describe('Header', () => {
       expect(wHeader).toHaveStyleRule('position', 'sticky');
       expect(wHeader).toHaveStyleRule('top', '0');
       expect(wHeader).toHaveStyleRule('z-index', '100');
-      expect(wHeader).toHaveStyleRule('height', '3.7rem');
-      expect(wHeader).toHaveStyleRule('padding', '0.5rem 1rem');
+
+      expect(wHeader).toHaveStyleRule('display', 'flex');
+      expect(wHeader).toHaveStyleRule('align-items', 'center');
+
+      expect(wHeader).toHaveStyleRule('min-height', '5rem');
+      expect(wHeader).toHaveStyleRule('padding', '0.5rem 1.5rem');
+
       expect(wHeader).toHaveStyleRule(
         'background-color',
-        theme.color.green.dark2
+        theme.color.green.dark3
       );
       expect(wHeader).toHaveStyleRule(
-        'border-bottom',
-        `1px solid ${theme.color.green.light}`
+        'filter',
+        `drop-shadow(0 0 2px ${theme.color.green.dark4})`
       );
+      expect(wHeader).toHaveStyleRule('opacity', '0.9');
     });
 
     test('in HeaderInner', () => {
       const wHeaderInner = wrapper.find('HeaderInner');
 
       expect(wHeaderInner).toHaveStyleRule('display', 'flex');
-      expect(wHeaderInner).toHaveStyleRule('align-items', 'center');
       expect(wHeaderInner).toHaveStyleRule('justify-content', 'space-between');
-      expect(wHeaderInner).toHaveStyleRule('height', '100%');
+      expect(wHeaderInner).toHaveStyleRule('width', '100%');
       expect(wHeaderInner).toHaveStyleRule('color', 'white');
     });
 
     test('in TitleWrapper', () => {
-      const wTitleWrapper = wrapper.find('TitleWrapper');
+      const wTitleWrapper = wrapper.find('S-TitleWrapper');
 
       expect(wTitleWrapper).toHaveStyleRule('display', 'flex');
-      expect(wTitleWrapper).toHaveStyleRule('align-items', 'center');
       expect(wTitleWrapper).toHaveStyleRule(
         'filter',
         new RegExp(
-          `drop-shadow\\((\\r?\\n)?(\\s+)2px 2px 1px ${theme.color.green.dark3.replace(
+          `drop-shadow\\((\\r?\\n)?(\\s+)2px 2px 1px ${theme.color.green.dark4.replace(
             /[()]/g,
             '\\$&'
           )}(\\r?\\n)?(\\s+)\\)`
         )
       );
+      expect(wTitleWrapper).toHaveStyleRule('opacity', '1');
     });
 
     test('in NavWrapper', () => {
-      const wNavWrapper = wrapper.find('NavWrapper');
+      const wNavWrapper = wrapper.find('S-NavWrapper');
 
       expect(wNavWrapper).toHaveStyleRule('display', 'flex');
-      expect(wNavWrapper).toHaveStyleRule('align-items', 'center');
       expect(wNavWrapper).toHaveStyleRule(
         'filter',
         new RegExp(
-          `drop-shadow\\((\\r?\\n)?(\\s+)2px 2px 1px ${theme.color.green.dark3.replace(
+          `drop-shadow\\((\\r?\\n)?(\\s+)2px 2px 1px ${theme.color.green.dark4.replace(
             /[()]/g,
             '\\$&'
           )}(\\r?\\n)?(\\s+)\\)`
         )
       );
+      expect(wNavWrapper).toHaveStyleRule('opacity', '1');
     });
   });
 });
