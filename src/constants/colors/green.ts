b7289006178aee6e87c57445d3ff84f5d28e79fa
8ayac <8ayac.y@gmail.com>
2021-03-01 01:52:54 +0900

    :art: Change the color constants definition.

--- src/constants/colors/green.ts
+++ src/constants/colors/green.ts
@@ -1,10 +1,10 @@
 export const green = {
-  base: 'hsla(148, 72%, 60%)',
-  light: 'hsla(148, 72%, 70%)',
-  light2: 'hsla(148, 72%, 80%)',
-  light3: 'hsla(148, 72%, 90%)',
-  dark: 'hsla(148, 72%, 50%)',
-  dark2: 'hsla(148, 72%, 40%)',
-  dark3: 'hsla(148, 72%, 30%)',
-  dark4: 'hsla(148, 72%, 20%)',
+  base: 'hsla(148, 72%, 55%)',
+  light: 'hsla(148, 72%, 65%)',
+  light2: 'hsla(148, 72%, 75%)',
+  light3: 'hsla(148, 72%, 85%)',
+  dark: 'hsla(148, 72%, 45%)',
+  dark2: 'hsla(148, 72%, 35%)',
+  dark3: 'hsla(148, 72%, 25%)',
+  dark4: 'hsla(148, 72%, 15%)',
 };
