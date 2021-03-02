565da8f4072572ff6b52ba07190c38bca61114fb
8ayac <8ayac.y@gmail.com>
2021-03-02 23:40:52 +0900

    :sparkles: Try using a experimental flag OptimizeFonts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,18 @@ import { globalStyle } from '@/src/shared/styles/global';
 import { Global, ThemeProvider } from '@emotion/react';
 import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
       <DefaultSeo {...nextSEOConfig} />
