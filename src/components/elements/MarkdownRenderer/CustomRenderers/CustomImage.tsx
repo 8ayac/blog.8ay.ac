487fdeda3ae02a00770a50e361331386a0316e1a
8ayac <8ayac.y@gmail.com>
2021-03-03 21:49:57 +0900

    :sparkles: Add custom image renderer to Markdown Renderer. To enable image optimization via the <Image /> component exported by next/image.

---
+++ src/components/elements/MarkdownRenderer/CustomRenderers/CustomImage.tsx
@@ -0,0 +1,33 @@
+import { config } from '@/site.config';
+import Image from 'next/Image';
+import React from 'react';
+import urljoin from 'url-join';
+
+export const CustomImage: React.FC<{
+  src: string;
+  alt?: string;
+  title?: string;
+}> = ({ src, alt, title }) => {
+  const srcUrl = /^https?:\/\/.*/.test(src)
+    ? src
+    : urljoin(config.site.rootUrl, src);
+  const { searchParams } = new URL(srcUrl);
+  const width = searchParams.get('w');
+  const height = searchParams.get('h');
+
+  return (
+    <>
+      {width && height ? (
+        <Image
+          src={src}
+          alt={alt}
+          title={title}
+          width={`${width}px`}
+          height={`${height}px`}
+        />
+      ) : (
+        <img src={src} alt={alt} title={title} />
+      )}
+    </>
+  );
+};
--- src/components/elements/MarkdownRenderer/CustomRenderers/index.tsx
+++ src/components/elements/MarkdownRenderer/CustomRenderers/index.tsx
@@ -1,7 +1,9 @@
+import { CustomImage } from '@/src/components/elements/MarkdownRenderer/CustomRenderers/CustomImage';
 import { FootnoteDefinition } from '@/src/components/elements/MarkdownRenderer/CustomRenderers/FootnoteDefinition';
 import { FootnoteReference } from '@/src/components/elements/MarkdownRenderer/CustomRenderers/FootnoteReference';
 
 export const customRenderers = {
   footnoteDefinition: FootnoteDefinition,
   footnoteReference: FootnoteReference,
+  image: CustomImage,
 };
